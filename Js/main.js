dfc8c2d6dd20dbe74dcc57d0a71873a9de6fecf0
kmahalakshmi <kkjanani2010@gmail.com>
2017-01-29 12:56:40 +0530

    files added

--- Js/main.js
+++ Js/main.js
@@ -4,24 +4,25 @@
 
 angular.module("MahaBlog",[
 	"ngRoute"
-]).config(['$routeProvider','$locationProvider',function($routeProvider, $locationProvider) {
+]).config(['$routeProvider','$locationProvider',function($routeProvider, $locationProvider) {	
 	$locationProvider.html5Mode({
 		enable: true,
 		requireBase: false
 	})
 
-}]).controller('HomeController', ['$scope', function($scope){
+}]).controller('HomeController', ['$scope', function($scope, $window){
 	$scope.state = "home";
 
 	$scope.changeRoute = function(route) {
 		$scope.state = route
+		//$window.location.href(route);
 	}
 
 	$scope.nav = [
-		{name:"About", slug: "about"},
-		{name:"Blog", slug: "blog"},
-		{name:"Projects", slug: "projects"},
-		{name:"Contact", slug: "contact"}
+		{name:"About", slug: "#about"},
+		{name:"Blog", slug: "https://medium.com/@Mahalakshmi_K"},
+		{name:"Projects", slug: "#projects"},
+		{name:"Contact", slug: "#contact"}
 	];
 }])
 
